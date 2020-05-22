512dead54041c74635004f4b5b374c5aea463df0
maquispe <62685159+maquispe@users.noreply.github.com>
2020-05-21 21:19:25 -0700

    organizes assets folder

--- Assets/script.js
+++ Assets/script.js
@@ -20,10 +20,5 @@ $(".btn").on("click", function (event) {
             newDiv.append("<h3>" + rest.name + "</h3><h5>" + rest.location.address + "</h5><p>" + rest.timings + "</p><img src='https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s(" + restLong + "," + restLat + ")/" + restLong + "," + restLat + ",14,0,0/300x200?access_token=pk.eyJ1IjoibWFxdWlzcGUiLCJhIjoiY2thaGw0YTd6MGtwcjJwdDFmYWkxaDd5dyJ9.QBCG28Weu55ywGM67a3G_Q'><hr class='my-4'>");
             $(".jumbotron").append(newDiv);
         }
-        
-        
-        
-
-
     })
 })
