099973c56e11605c38d306ec11455b0ce920c403
maquispe <62685159+maquispe@users.noreply.github.com>
2020-05-21 21:14:41 -0700

    adds API functionality

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ $(".btn").on("click", function (event) {
         method: "GET"
     }).then(function (response) {
         console.log(response)
-        $(".container").empty();
+        $(".jumbotron").empty();
+        var results = response.restaurants;
+        for(var i = 0; i < results.length / 2; i++){
+            var rest = response.restaurants[i].restaurant
+            var restLat = rest.location.latitude;
+            var restLong = rest.location.longitude;
+            var newDiv = $("<div>");
+            newDiv.append("<h3>" + rest.name + "</h3><h5>" + rest.location.address + "</h5><p>" + rest.timings + "</p><img src='https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-s(" + restLong + "," + restLat + ")/" + restLong + "," + restLat + ",14,0,0/300x200?access_token=pk.eyJ1IjoibWFxdWlzcGUiLCJhIjoiY2thaGw0YTd6MGtwcjJwdDFmYWkxaDd5dyJ9.QBCG28Weu55ywGM67a3G_Q'><hr class='my-4'>");
+            $(".jumbotron").append(newDiv);
+        }
+        
+        
         
 
 
