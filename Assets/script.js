c369ef13b27640d67dea99a7d03d9f5d66b18e42
maquispe <62685159+maquispe@users.noreply.github.com>
2020-05-25 23:38:27 -0700

    Changes welcome prompt

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,22 @@
 $(".btn").on("click", function (event) {
     event.preventDefault();
-    
-    
     var zipCode = $("#zip").val();
+   
     $.ajax({
         url: "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDgBivFUE9d6ctIZDBOhP6_ZntlaH2CYoU&address=" + zipCode,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
+        //console.log(response);
         var coordsLat = response.results[0].geometry.location.lat 
         var coordsLong = response.results[0].geometry.location.lng
         var cuisines = $("#cuisine").val()
         var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + coordsLat + "&lon=" + coordsLong + "&q=" + cuisines + "&apikey=7ad2112e89a393cbb0ebbe7f844fe015";
-        console.log(queryURL);
+        //console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            //console.log(response)
             $(".jumbotron").empty();
             var results = response.restaurants;
             for(var i = 0; i < results.length / 2; i++){
